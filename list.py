7580219f9fb1287052c89b941edef2862d853bbc
Vidya Akbar <vidyaakbar0608@gmail.com>
2021-04-23 08:38:38 +0530

    Update from pycharm

--- list.py
+++ list.py
@@ -1,11 +1,24 @@
 # second smallest element
 
-def find_length(list):
-    length = len(list)
-    list.sort()
-    print("second smallest element is ", list[1])
+def find_length(lists):
+    length = len(lists)
+    lists.sort()
+    print("second smallest element is ", lists[1])
 
 
-list = [1, 2, 3, 4]
-largest = find_length(list)
+lists = [1, 2, 3, 4]
+find_length(list)
+
+# change nth value with (n+1)th value
+
+
+list1 = [0, 1, 2, 3, 4, 5]
+n = 2
+new_list = []
+for i in range (0, len(list1), n):
+    new_list.append(list1[i+1])
+    new_list.append(list1[i])
+
+print(new_list)
+
 
