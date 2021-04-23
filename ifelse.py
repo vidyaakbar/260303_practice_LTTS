a3f8cfc78d1836472e832dda7fa28f6ee2c9f15d
Vidya Akbar <vidyaakbar0608@gmail.com>
2021-04-23 09:16:18 +0530

    Update from pycharm

---
+++ ifelse.py
@@ -0,0 +1,33 @@
+# next date of give input date
+
+day = int(input("Enter the day :"))
+month = int(input("Enter the month :"))
+year = int(input("Enter the year :"))
+
+if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+    if month == 2:
+        no_of_days = 29
+    elif month in [4, 6, 9, 11]:
+        no_of_days = 30
+    else:
+        no_of_days = 31
+
+else:
+    if month == 2:
+        no_of_days = 28
+    elif month in [4, 6, 9, 11]:
+        no_of_days = 30
+    else:
+        no_of_days = 31
+
+if day < no_of_days:
+
+    print(day + 1, "-", month, "-", year)
+
+else:
+    if month == 12:
+        print(1, "-", 1, "-", year + 1)
+    else:
+        print(1, "-", month + 1, "-", year)
+
+
