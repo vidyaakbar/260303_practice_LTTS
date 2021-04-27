76ff5ec86951b927a56120c8f873a8b03d9cea62
Vidya Akbar <vidyaakbar0608@gmail.com>
2021-04-27 22:50:43 +0530

    Update from pycharm

--- prac/practisee.py
+++ prac/practisee.py
@@ -17,3 +17,12 @@ b = Juice('Apple', 2.0)
 
 
 print(a.__add__(b)())
+
+
+a = input("enter a number:")      # let's enter 10
+b = input("enter a number:")      # let's enter 20
+res = a + b
+print(res)
+print (type(res))
+print(type(a))
+print(type(b))
